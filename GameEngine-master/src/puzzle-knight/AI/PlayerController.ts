3d7d826065fcfdde50512b0b965ecafc0092db3c
KyrieErwin <59181361+tommylin121314@users.noreply.github.com>
2021-04-10 23:16:35 -0400

    Create PlayerController.ts with movement. Also created BattlerAI.ts for enemies and player. Created an initialize player method to initialize the player

---
+++ GameEngine-master/src/puzzle-knight/AI/PlayerController.ts
@@ -0,0 +1,83 @@
+import Vec2 from "../../Wolfie2D/DataTypes/Vec2";
+import GameEvent from "../../Wolfie2D/Events/GameEvent";
+import Input from "../../Wolfie2D/Input/Input";
+import AnimatedSprite from "../../Wolfie2D/Nodes/Sprites/AnimatedSprite";
+import AI from "../../Wolfie2D/DataTypes/Interfaces/AI";
+import BattlerAI from "./BattlerAI";
+
+export default class PlayerController implements BattlerAI{
+    health: number
+    owner: AnimatedSprite
+
+    //movement
+    private direction: Vec2
+    private speed: number
+    private lookDir: Vec2
+    private lastLookedRight: boolean
+
+    initializeAI(owner: AnimatedSprite, options: Record<string, any>) {
+        this.owner = owner;
+        this.direction = new Vec2(0, 0);
+        this.lookDir = new Vec2(0, 0);
+        this.speed = options.speed;
+    }
+
+    activate(options: Record<string, any>) {    }
+
+    handleEvent(event: GameEvent) {    }
+
+    update(deltaT: number): void {
+
+        console.log(this.speed);
+        //Updates direction of movement
+        let xInput, yInput;
+        if(Input.isKeyPressed("a") && !Input.isKeyPressed("d")) {
+            xInput = -1;
+            this.lastLookedRight = false;
+        }
+        else if(Input.isKeyPressed("d") && !Input.isKeyPressed("a")) {
+            xInput = 1;
+            this.lastLookedRight = true;
+        }
+        else {
+            xInput = 0;
+        }
+        if(Input.isKeyPressed("w") && !Input.isKeyPressed("s")) {
+            yInput = -1;
+        }
+        else if(Input.isKeyPressed("s") && !Input.isKeyPressed("w")) {
+            yInput = 1;
+        }
+        else {
+            yInput = 0;
+        }
+        this.direction = new Vec2(xInput, yInput);
+
+        if(this.lastLookedRight) {
+            console.log("flip");
+            this.owner.invertX = false;
+        }
+        else {
+            this.owner.invertX = true;
+        }
+
+        //Moves player
+        if(!this.direction.isZero()) {
+            console.log("MOVING");
+            this.owner.move(this.direction.normalized().scale(this.speed * deltaT));
+            this.owner.animation.playIfNotAlready("WALK");
+        }
+        else {
+            this.owner.animation.playIfNotAlready("IDLE");
+        }
+    }
+
+    damage(damage: number): void {
+        this.health -= damage;
+        if(this.health <= 0) {
+            console.log("PLAYER KILLED");
+        }
+    }
+
+    destroy() {    }
+}
--- GameEngine-master/src/puzzle-knight/Scenes/TestDungeon.ts
+++ GameEngine-master/src/puzzle-knight/Scenes/TestDungeon.ts
@@ -11,6 +11,7 @@ import Label from "../../Wolfie2D/Nodes/UIElements/Label";
 import { UIElementType } from "../../Wolfie2D/Nodes/UIElements/UIElementTypes";
 import Color from "../../Wolfie2D/Utils/Color";
 import Input from "../../Wolfie2D/Input/Input";
+import PlayerController from "../AI/playerController";
 
 export default class TestDungeon extends Scene {
 
@@ -25,6 +26,7 @@ export default class TestDungeon extends Scene {
 
     loadScene() {
         this.load.spritesheet("skeletonArcher", "assets/spritesheets/skeletonArcher.json");
+        this.load.spritesheet("knight", "assets/spritesheets/knight.json");
         this.load.tilemap("level", "assets/tilemaps/test-dungeon.json");
     }
 
@@ -45,6 +47,23 @@ export default class TestDungeon extends Scene {
         this.skeletonArcher.position.set(5*32, 5*32);
         this.skeletonArcher.animation.play("WALK");
         console.log(this.skeletonArcher);
+
+        //Initializes player
+        this.initializePlayer();
+        this.viewport.follow(this.player);
+        this.viewport.setZoomLevel(3);
+    }
+
+    initializePlayer() {
+        this.player = this.add.animatedSprite("knight", "primary");
+        this.player.position.set(7*32, 5*32);
+        this.player.animation.play("IDLE");
+        this.player.addPhysics(new AABB(Vec2.ZERO, new Vec2(5, 5)));
+        this.player.addAI(PlayerController,
+            {
+                speed: 75
+            }
+        )
     }
 
     updateScene() {

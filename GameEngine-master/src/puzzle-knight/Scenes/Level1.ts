dd2c8bed9c6174eb9ee00dc428ec83fc2c5c633d
KyrieErwin <59181361+tommylin121314@users.noreply.github.com>
2021-04-19 20:50:19 -0400

    Co-authored-by: henryhe320 <henryhe320@users.noreply.github.com>

--- GameEngine-master/src/puzzle-knight/Scenes/Level1.ts
+++ GameEngine-master/src/puzzle-knight/Scenes/Level1.ts
@@ -4,15 +4,15 @@ import GameLevel from "./GameLevel";
 
 export default class Level1 extends GameLevel {
 
-    private skeletonPos = [new Vec2(50, 50), new Vec2(200,400), new Vec2(60,70), new Vec2(2, 2), new Vec2(19, 4), new Vec2(31, 2), new Vec2(47, 30), new Vec2(18, 39), new Vec2(40, 39), new Vec2(46, 47), new Vec2(13, 23), new Vec2(7, 35), new Vec2(2, 18), new Vec2(13, 40), new Vec2(13, 46), new Vec2(4, 40), new Vec2(4, 46)];
-    private goblinPos = [new Vec2(100,100), new Vec2(600,600), new Vec2(3, 4), new Vec2(10, 3), new Vec2(22, 2), new Vec2(40, 11), new Vec2(32, 13), new Vec2(38, 17), new Vec2(40, 25), new Vec2(25, 29),, new Vec2(33, 25), new Vec2(33, 29),, new Vec2(28, 44), new Vec2(45, 39), new Vec2(17, 35)];
-    private healthpotsPos = [new Vec2(75,80), new Vec2(120,150), new Vec2(200,350)];
+    private skeletonPos = [new Vec2(2, 2), new Vec2(19, 4), new Vec2(31, 2), new Vec2(47, 30), new Vec2(18, 39), new Vec2(40, 39), new Vec2(46, 47), new Vec2(13, 23), new Vec2(7, 35), new Vec2(2, 18), new Vec2(13, 40), new Vec2(13, 46), new Vec2(4, 40), new Vec2(4, 46)];
+    private goblinPos = [new Vec2(3, 4), new Vec2(10, 3), new Vec2(22, 2), new Vec2(40, 11), new Vec2(32, 13), new Vec2(38, 17), new Vec2(40, 25), new Vec2(25, 29),, new Vec2(33, 25), new Vec2(33, 29),, new Vec2(28, 44), new Vec2(45, 39), new Vec2(17, 35)];
+    private healthpotsPos = [new Vec2(1, 1), new Vec2(25,1), new Vec2(36,23), new Vec2(1,17), new Vec2(19,35), new Vec2(40,39), new Vec2(46,42)];
 
     private walls: OrthogonalTilemap;
 
     loadScene() {
         super.loadScene();
-        this.load.tilemap("level", "assets/tilemaps/test-dungeon.json");
+        this.load.tilemap("level", "assets/tilemaps/level_1.json");
         
     }
 
@@ -21,7 +21,7 @@ export default class Level1 extends GameLevel {
     }
 
     startScene() {
-        this.playerSpawn = new Vec2(200,200);
+        this.playerSpawn = new Vec2(47,2);
 
         super.startScene();
 
--- GameEngine-master/src/puzzle-knight/Scenes/MainMenu.ts
+++ GameEngine-master/src/puzzle-knight/Scenes/MainMenu.ts
@@ -58,3 +58,5 @@ export default class MainMenu extends Scene {
 
 
         //////****** CONTROLS *******///////
+    }
+}

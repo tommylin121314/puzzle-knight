77b4900e62638db81e28bab073df3dba4b010214
henryhe320 <henry.he@stonybrook.edu>
2021-05-17 06:40:48 -0400

    refined ice level 2

--- GameEngine-master/src/puzzle-knight/Scenes/Ice2.ts
+++ GameEngine-master/src/puzzle-knight/Scenes/Ice2.ts
@@ -5,6 +5,7 @@ import Vec2 from "../../Wolfie2D/DataTypes/Vec2";
 import Rect from "../../Wolfie2D/Nodes/Graphics/Rect";
 import DragonEntrance from "./DragonEntrance";
 import { GameEventType } from "../../Wolfie2D/Events/GameEventType";
+import PlayerController from "../AI/Player/PlayerController";
 
 export default class Ice2 extends GameLevel {
     private goblinPos = [new Vec2(13, 22), new Vec2(11, 21), new Vec2(5, 10), new Vec2(6, 8), new Vec2(8, 9), new Vec2(17, 9), new Vec2(19, 13)];
@@ -52,6 +53,7 @@ export default class Ice2 extends GameLevel {
         });
 
         this.initPlayer();
+        this.viewport.setCenter(600,400);
         this.initViewport();
         this.initDialogueUI();
 
@@ -88,15 +90,30 @@ export default class Ice2 extends GameLevel {
 
         this.addDoor();
 
+        this.overlay.position = new Vec2(this.viewport.getCenter().x/2, this.viewport.getCenter().y/2)
+
         this.forced = [new Rect(new Vec2(25 * 32, 47 * 32), new Vec2(64, 64)), new Rect(new Vec2(20 * 32, 39 * 32), new Vec2(40, 40))];
         this.optional = [];
         this.dialogueList = [
             ["Entering Ice Dungeon: Chamber Two..."],
-            ["The ice breaks after you step on it. Tread carefully."]
+            ["Break all the ice by walking on it. Tread carefully."]
         ];
+        (<PlayerController>this.player.ai).walls.setTileAtRowCol(new Vec2(30, 39), 6);
     }
     updateScene(deltaT: number) {
         super.updateScene(deltaT);
+
+        // open right wall of main room if all keys collected
+        if(this.hasDoor) {
+            if(this.keysCollected < this.numKeys) {
+                if ((<PlayerController>this.player.ai).walls.getTileAtRowCol(new Vec2(30, 39)) == 6) return;
+                (<PlayerController>this.player.ai).walls.setTileAtRowCol(new Vec2(30, 39), 6);
+            }
+            else {
+                if ((<PlayerController>this.player.ai).walls.getTileAtRowCol(new Vec2(30, 39)) == 0) return;
+                (<PlayerController>this.player.ai).walls.setTileAtRowCol(new Vec2(30, 39), 0);
+            }
+        }
     }
 
     // handleIceBreak() {
